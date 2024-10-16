b4f6f620cf90db94c648a0cb26a65b5c2e5101c6
WarStalkeR <stalker6000@hotmail.com>
2024-10-16 20:59:48 +0300

    Minor ModLog changes.

--- FFU_BR/Helpers/ModLog.cs
+++ FFU_BR/Helpers/ModLog.cs
@@ -3,25 +3,25 @@
 namespace FFU_Beyond_Reach {
     public static class ModLog {
         public static void Init() {
-            UnityEngine.Debug.Log($"[Mod:    Init] {FFU_BR_Defs.ModName} v{FFU_BR_Defs.ModVersion}, {DateTime.Now}");
+            UnityEngine.Debug.Log($"Initialized: {FFU_BR_Defs.ModName} v{FFU_BR_Defs.ModVersion}, {DateTime.Now}");
         }
         public static void Info(string logEntry = "") {
-            UnityEngine.Debug.Log($"[Mod:    Info] {logEntry}");
+            UnityEngine.Debug.Log($"{logEntry}");
         }
         public static void Debug(string logEntry = "") {
-            UnityEngine.Debug.Log($"[Mod:   Debug] {logEntry}");
+            UnityEngine.Debug.Log($"{logEntry}");
         }
         public static void Message(string logEntry = "") {
-            UnityEngine.Debug.LogWarning($"[Mod: Message] {logEntry}");
+            UnityEngine.Debug.LogWarning($"{logEntry}");
         }
         public static void Warning(string logEntry = "") {
-            UnityEngine.Debug.LogWarning($"[Mod: Warning] {logEntry}");
+            UnityEngine.Debug.LogWarning($"{logEntry}");
         }
         public static void Error(string logEntry = "") {
-            UnityEngine.Debug.LogError($"[Mod:   Error] {logEntry}");
+            UnityEngine.Debug.LogError($"{logEntry}");
         }
         public static void Fatal(string logEntry = "") {
-            UnityEngine.Debug.LogError($"[Mod:   Fatal] {logEntry}");
+            UnityEngine.Debug.LogError($"{logEntry}");
         }
     }
 }
