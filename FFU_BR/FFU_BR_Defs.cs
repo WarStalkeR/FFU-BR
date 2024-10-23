a6bd461e620ae634add7b14d6a73bec963784749
WarStalkeR <stalker6000@hotmail.com>
2024-10-23 19:18:54 +0300

    Updated version and removed example code.

--- FFU_BR/FFU_BR_Defs.cs
+++ FFU_BR/FFU_BR_Defs.cs
@@ -15,7 +15,7 @@ using System.IO;
 namespace FFU_Beyond_Reach {
     public class FFU_BR_Defs {
         public static readonly string ModName = "BepInEx: Beyond Reach";
-        public static readonly string ModVersion = "0.1.0.0";
+        public static readonly string ModVersion = "0.1.5.0";
 
         private static ConfigFile ModDefs = null;
         public static SyncLogs SyncLogging = SyncLogs.None;
@@ -102,82 +102,3 @@ namespace FFU_Beyond_Reach {
         }
     }
 }
-
-/*
-namespace PatchTargetNamespace {
-    public class TargetClassName {
-        // ----------------------------------------------------------- //
-        // Class for example purpose only. Doesn't exist in your code. //
-        // Only in targeted namespace of DLL you want to modify.       //
-        // If target is in global namespace (-) in ILSpy/DnSpy then,   //
-        // patch_Target should be outside all namespaces in the code.  //
-        // ----------------------------------------------------------- //
-        public int a;
-        public string b;
-        public TargetClassName(int refA, string refB) {
-            a = refA;
-            b = refB;
-        }
-    }
-    public class patch_TargetClassName : TargetClassName {
-        // ------------------------------------------------------ //
-        // Example: a way to access private variable or function. //
-        // ------------------------------------------------------ //
-        [MonoModIgnore] private bool privateVar;
-        [MonoModIgnore] private bool privateFunc() { return true; }
-        // ------------------------------------------- //
-        // Example: a way to modify existing function. //
-        // ------------------------------------------- //
-        public extern bool orig_TargetFunctionToModify();
-        public bool TargetFunctionToModify() {
-            // The code you run before original function.
-            var moddedResult = orig_TargetFunctionToModify();
-            // The code you run after original function.
-            return moddedResult;
-        }
-        // ---------------------------------------------- //
-        // Example: a way to overwrite existing function. //
-        // ---------------------------------------------- //
-        [MonoModReplace]
-        public bool FunctionToReplace() {
-            // Careful, if it has multiple constructors, you might need to give up on MonoModReplace.
-            return true;
-        }
-        // ---------------------------------------------- //
-        // Example: a way to modify existing constructor. //
-        // ---------------------------------------------- //
-        [MonoModIgnore] public patch_TargetClassName(int refC, string refD) : base(refC, refD) { }
-        // You can remove [MonoModIgnore] row, if compiler doesn't throw error without it.
-        [MonoModOriginal] public extern void orig_TargetConstructor(int refA, string refB);
-        [MonoModConstructor]
-        public void TargetConstructor(int refA, string refB) {
-            orig_TargetConstructor(refA, refB);
-            // Here you can initialize variables you've added.
-            // You also can do it before original constructor initialized.
-            // Don't forget to use it in exactly same namespace as original.
-        }
-        // ------------------------------------------------ //
-        // Example: a way to overwrite existing enumerator. //
-        // ------------------------------------------------ //
-        [MonoModEnumReplace]
-        public enum patch_TargetEnum_Replace {
-            // Don't forget to add original entires used by the code.
-            // Or you might end up with whole bag of errors that make no sense.
-            origEnumA,
-            origEnumB,
-            origEnumC,
-            modEnumD
-        }
-        // ------------------------------------------------ //
-        // Example: add new entries to existing enumerator. //
-        // ------------------------------------------------ //
-        public enum patch_TargetEnum_Extend {
-            // Ensure that new entries use numbers that way above original.
-            // In case, if original enumerator will get more entries.
-            modEnumX = 1337,
-            modEnumY = 1338,
-            modEnumZ = 1339
-        }
-    }
-}
-*/
