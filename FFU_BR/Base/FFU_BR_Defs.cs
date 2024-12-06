66e0b6208290d91683ec4712b106425199ba3c86
WarStalkeR <stalker6000@hotmail.com>
2024-12-06 17:58:56 +0200

    Compatibility update.

--- FFU_BR/Base/FFU_BR_Defs.cs
+++ FFU_BR/Base/FFU_BR_Defs.cs
@@ -16,7 +16,7 @@ using System.IO;
 namespace FFU_Beyond_Reach {
     public partial class FFU_BR_Defs {
         public static readonly string ModName = "Fight For Universe: Beyond Reach";
-        public static readonly string ModVersion = "0.4.5.0";
+        public static readonly string ModVersion = "0.4.5.5";
 
         private static ConfigFile ModDefs = null;
         public static SyncLogs SyncLogging = SyncLogs.None;
